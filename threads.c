b86b0ac85d4c04a4ba4834000a0334a277bd23de
Buket Çolak <buket@Buket-MacBook-Air.local>
2025-03-15 14:10:09 +0300

    almost

--- threads.c
+++ threads.c
@@ -6,7 +6,7 @@
 /*   By: buket <buket@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 19:32:47 by bucolak           #+#    #+#             */
-/*   Updated: 2025/03/14 23:16:11 by buket            ###   ########.fr       */
+/*   Updated: 2025/03/15 14:07:06 by buket            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,34 +28,37 @@ void	meal_order(t_philo *philo, int left_id, int right_id)
 {
 	if((philo->id==1 || philo->id==philo->data->num_of_philo) && philo->data->num_of_philo%2==1)
 	{
-		
-	pthread_mutex_lock(&philo->data->d2_lock);
-	while((philo->id==1 && philo->data->two!=1) 
-		|| (philo->id==philo->data->num_of_philo && philo->data->two!=0))
+		while (1)
 		{
-			pthread_mutex_unlock(&philo->data->d2_lock);
-			ft_usleep(500);
+			if(cont_dead(philo)==1) return ;
 			pthread_mutex_lock(&philo->data->d2_lock);
+			if ((philo->id == 1 && philo->data->two == 1) ||
+				(philo->id == philo->data->num_of_philo && philo->data->two == 0))
+			{
+				pthread_mutex_unlock(&philo->data->d2_lock);
+				break;
+			}
+		pthread_mutex_unlock(&philo->data->d2_lock);
+		ft_usleep(500);
 		}
-	pthread_mutex_unlock(&philo->data->d2_lock);
 	}
+	if(cont_dead(philo)==1) return ;
 	pthread_mutex_lock(&philo->data->forks[right_id]);
 	pthread_mutex_lock(&philo->data->forks[left_id]);
-	if(cont_dead(philo)==1) return ;
 	printf("%lld %d has taken a fork\n", get_time()
 		- philo->data->start_time, philo->id);
 	printf("%lld %d has taken a fork\n", get_time()
 		- philo->data->start_time, philo->id);
 	if((philo->id==1 || philo->id==philo->data->num_of_philo)  && philo->data->num_of_philo%2==1)
 	{
-		
-	pthread_mutex_lock(&philo->data->d2_lock);
-	if(philo->id==1)
-		philo->data->two=1;
-	else if(philo->id==philo->data->num_of_philo)
-		philo->data->two=0;
-	pthread_mutex_unlock(&philo->data->d2_lock);
+		pthread_mutex_lock(&philo->data->d2_lock);
+		if(philo->id==1)
+			philo->data->two=1;
+		else if(philo->id==philo->data->num_of_philo)
+			philo->data->two=0;
+		pthread_mutex_unlock(&philo->data->d2_lock);
 	}
+	if(cont_dead(philo)==1) return ;
 	pthread_mutex_lock(&philo->data->d_lock);
 	philo->last_meal_time = get_time();
 	philo->meals_eaten++;
@@ -69,13 +72,12 @@ void	meal_order(t_philo *philo, int left_id, int right_id)
 	pthread_mutex_unlock(&philo->data->forks[left_id]);
 	if((philo->id==1 || philo->id==philo->data->num_of_philo) && philo->data->num_of_philo%2==1)
 	{
-		
-	pthread_mutex_lock(&philo->data->d2_lock);
-	if(philo->id==1)
-		philo->data->two=0;
-	else if(philo->id==philo->data->num_of_philo)
-		philo->data->two=1;
-	pthread_mutex_unlock(&philo->data->d2_lock);
+		pthread_mutex_lock(&philo->data->d2_lock);
+		if(philo->id==1)
+			philo->data->two=0;
+		else if(philo->id==philo->data->num_of_philo)
+			philo->data->two=1;
+		pthread_mutex_unlock(&philo->data->d2_lock);
 	}
 }
 
